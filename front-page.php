92a79ad131b92c09c3e8fafc7a43f89fa4777c34
Ritchie Jacobs <jacobs.ritchie@gmail.com>
2014-10-20 15:20:20 +0200

    sections + titles

---
+++ front-page.php
@@ -0,0 +1,29 @@
+<!-- Intro -->
+<div class="section" id="intro">
+	<h1><?php echo roots_title(); ?></h1>
+</div>
+
+<!-- About -->
+<div class="section" id="about">
+	<h2><?php the_field('title_about'); ?></h2>
+</div>
+
+<!-- Process -->
+<div class="section" id="process">
+	<h2><?php the_field('title_process'); ?></h2>
+</div>
+
+<!-- Work -->
+<div class="section" id="work">
+	<h2><?php the_field('title_work'); ?></h2>
+</div>
+
+<!-- Team -->
+<div class="section" id="team">
+	<h2><?php the_field('title_team'); ?></h2>
+</div>
+
+<!-- Contact -->
+<div class="section" id="contact">
+	<h2><?php the_field('title_contact'); ?></h2>
+</div>
