8a8c8b8f742fcb7fd9645b063973a5dfea9efa24
unknown <silent785@hotmail.com>
2014-10-23 15:06:10 +0200

    Removed container class

--- header.php
+++ header.php
@@ -20,7 +20,7 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="hfeed site container">
+<div id="page" class="hfeed site">
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'designosource' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
