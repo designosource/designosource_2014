0f6ff13280631b2bcdd45ae08da04179c931e492
Ritchie Jacobs <jacobs.ritchie@gmail.com>
2014-10-23 16:30:02 +0200

    jquery + bootstrap script

--- footer.php
+++ footer.php
@@ -19,7 +19,8 @@
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
-<?php wp_footer(); ?>
-
+<!-- <?php wp_footer(); ?> -->
+	<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 </body>
 </html>
