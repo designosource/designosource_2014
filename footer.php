29d1e7ecf4e4ab310851819c6ab4a7f6aab1d647
Ritchie Jacobs <jacobs.ritchie@gmail.com>
2014-10-28 17:57:14 +0100

    update footer info

--- footer.php
+++ footer.php
@@ -11,15 +11,11 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'designosource' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'designosource' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'designosource' ), 'designosource', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
-		</div><!-- .site-info -->
+		<span>Made by the designosource 2014-2015 team</span>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
-<!-- <?php wp_footer(); ?> -->
+	<?php wp_footer(); ?>
 	<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
 	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 </body>
