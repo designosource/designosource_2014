4553569f51aa8b7b551b6df8bf19dbbeb4f6e7cb
Ritchie Jacobs <jacobs.ritchie@gmail.com>
2014-11-21 21:59:14 +0100

    URL fix

--- footer.php
+++ footer.php
@@ -16,8 +16,8 @@
 </div><!-- #page -->
 
 	<?php wp_footer(); ?>
-	<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+	<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 	<script src="<?php bloginfo('template_url'); ?>/js/script.js"></script>
 </body>
