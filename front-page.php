5e1ebd7682654a2e2200d52cb1ea6a4ef1cac39a
Ritchie Jacobs <jacobs.ritchie@gmail.com>
2014-11-26 20:20:34 +0100

    front page video fix

--- front-page.php
+++ front-page.php
@@ -17,10 +17,10 @@ get_header(); ?>
 
 			<div class="section navsection" id="intro">
 				<div class="video-wrapper">
-					<video autoplay poster="http://designosource.be/2014/designosource/wp-content/themes/designosource_2014/assets/team.jpg">
-						<source src="http://designosource.be/2014/designosource/wp-content/themes/designosource_2014/assets/team_movie.m4v" type="video/mp4">
+					<video autoplay>
+						<source src="http://designosource.be/2014/designosource/wp-content/themes/designosource_2014/assets/team_movie.mp4" type="video/mp4">
 						<source src="http://designosource.be/2014/designosource/wp-content/themes/designosource_2014/assets/team_movie.webm" type="video/webm">
-						<source src="http://designosource.be/2014/designosource/wp-content/themes/designosource_2014/assets/team_movie.ogg" type="video/ogg">
+						<source src="http://designosource.be/2014/designosource/wp-content/themes/designosource_2014/assets/team_movie.ogv" type="video/ogg">
 					</video>
 				</div>
 				
