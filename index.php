1bdc0f88f8527f15cc6c02a23e878b1ce676d464
Ritchie Jacobs <jacobs.ritchie@gmail.com>
2014-12-14 22:04:59 +0100

    added blog grid classes

--- index.php
+++ index.php
@@ -13,38 +13,53 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div class="container">
+	<div class="row">
+		<div class="col-md-8">
+			<div id="primary" class="content-area">
+				<main id="main" class="site-main" role="main">
 
-		<?php if ( have_posts() ) : ?>
+					<?php if ( have_posts() ) : ?>
 
-			<div class="blog-overview container">
-				<h1 class="overview-title">Blog</h1>
+					<div class="blog-overview">
+						<h1 class="overview-title">Designo-blog</h1>
+						<p class="overview-intro">We houden je hier op de hoogte van de online trends en best practices.</p>
 
-				<?php /* Start the Loop */ ?>
-				<?php while ( have_posts() ) : the_post(); ?>
+						<?php /* Start the Loop */ ?>
+						<?php while ( have_posts() ) : the_post(); ?>
 
-					<?php
-						/* Include the Post-Format-specific template for the content.
-						 * If you want to override this in a child theme, then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						get_template_part( 'content', get_post_format() );
-					?>
+							<?php
+								/* Include the Post-Format-specific template for the content.
+								 * If you want to override this in a child theme, then include a file
+								 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+								 */
+								get_template_part( 'content', get_post_format() );
+							?>
 
-				<?php endwhile; ?>
+						<?php endwhile; ?>
 
-				<?php designosource_paging_nav(); ?>
+						<?php designosource_paging_nav(); ?>
 
-				<?php else : ?>
+						<?php else : ?>
 
-					<?php get_template_part( 'content', 'none' ); ?>
+							<?php get_template_part( 'content', 'none' ); ?>
 
-				<?php endif; ?>
-			</div>
+						<?php endif; ?>
+					</div>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+				</main><!-- #main -->
+			</div><!-- #primary -->
+		</div>
 
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+		<div class="col-md-4">
+			<?php get_sidebar(); ?>
+		</div>
+
+		
+
+		
+		<?php get_footer(); ?>
+	</div>
+</div>
+
+	
