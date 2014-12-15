0dfba54f8cb1720e7e46d45439e459afab8e761e
Ritchie Jacobs <jacobs.ritchie@gmail.com>
2014-12-15 20:03:42 +0100

    standaard commentaar formulier aangepast voor Bootstrap

--- comments.php
+++ comments.php
@@ -64,6 +64,42 @@ if ( post_password_required() ) {
 		<p class="no-comments"><?php _e( 'Comments are closed.', 'designosource' ); ?></p>
 	<?php endif; ?>
 
-	<?php comment_form(); ?>
+	<?php
+		/* BLOG COMMENT FORM (adapted for Bootstrap) */
+		$fields =  array(
+		  'author' =>
+		    '<p class="comment-form-author"><label for="author">' . __( 'Name', 'domainreference' ) . '</label> ' .
+		    ( $req ? '<span class="required">*</span>' : '' ) .
+		    '<input class="form-control" id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) .
+		    '" size="30"' . $aria_req . ' /></p>',
+		  'email' =>
+		    '<p class="comment-form-email"><label for="email">' . __( 'Email', 'domainreference' ) . '</label> ' .
+		    ( $req ? '<span class="required">*</span>' : '' ) .
+		    '<input class="form-control" id="email" name="email" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) .
+		    '" size="30"' . $aria_req . ' /></p>',
+		  'url' =>
+		    '<p class="comment-form-url"><label for="url">' . __( 'Website', 'domainreference' ) . '</label>' .
+		    '<input class="form-control" id="url" name="url" type="text" value="' . esc_attr( $commenter['comment_author_url'] ) .
+		    '" size="30" /></p>',
+		);
+
+		$args = array(
+	        // change the title of send button 
+	        'label_submit'=>'Verstuur',
+	        // change the title of the reply section
+	        'title_reply'=>'Write a Reply or Comment',
+	        // remove "Text or HTML to be displayed after the set of comment fields"
+	        'comment_notes_after' => '',
+	        'fields' => apply_filters( 'comment_form_default_fields', $fields ),
+	        // redefine your own textarea (the comment body)
+	        'comment_field' => '<p class="comment-form-comment"><label for="comment">' . _x( 'Comment', 'noun' ) . '</label><br /><textarea class="form-control" id="comment" name="comment" aria-required="true"></textarea></p>',
+		);
+
+		ob_start();
+		comment_form( $args );
+		$form = ob_get_clean(); 
+		$form = str_replace('class="comment-form"','class="comment-form"', $form);
+		echo str_replace('id="submit"','class="btn btn-primary"', $form);
+	?>
 
 </div><!-- #comments -->
