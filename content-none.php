dc70fcfd508122e02ef8b8c2471c7d639ed784be
Ritchie Jacobs <jacobs.ritchie@gmail.com>
2015-02-19 13:45:18 +0100

    translate search text

--- content-none.php
+++ content-none.php
@@ -10,7 +10,7 @@
 
 <section class="no-results not-found">
 	<header class="page-header">
-		<h1 class="page-title"><?php _e( 'Nothing Found', 'designosource' ); ?></h1>
+		<h1 class="page-title"><?php _e( 'Geen resultaten', 'designosource' ); ?></h1>
 	</header><!-- .page-header -->
 
 	<div class="page-content">
@@ -20,13 +20,11 @@
 
 		<?php elseif ( is_search() ) : ?>
 
-			<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'designosource' ); ?></p>
-			<?php get_search_form(); ?>
+			<p><?php _e( 'Er kon niets gevonden worden. Probeer een andere zoekterm in te geven.', 'designosource' ); ?></p>
 
 		<?php else : ?>
 
 			<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'designosource' ); ?></p>
-			<?php get_search_form(); ?>
 
 		<?php endif; ?>
 	</div><!-- .page-content -->
